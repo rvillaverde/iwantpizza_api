64c3f9b321b2efc2fd0bc443dde7b2876a59d082
Romina Villaverde <romina.villaverde@gmail.com>
2020-04-26 10:53:00 -0500

    api endpoint to get product

--- server.js
+++ server.js
@@ -10,44 +10,25 @@ let app = express();
 
 const ProductService = require('./lib/productService.js');
 
-/*app.use(compression());
-app.use(lessMiddleware(__dirname + '/public',{
-  debug: true,
-  dest: __dirname,
-  force: true
-}));*/
 app.use(express.static(path.join(__dirname, 'public')));
-
-/*app.use("/assets", express.static(__dirname + '/assets'));
-app.use("/node_modules", express.static(__dirname + '/node_modules'));
-app.use("/styles", express.static(__dirname + '/styles'));
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/views', express.static(__dirname + '/views'));*/
-
-/*app.use(bodyParser.urlencoded({ extended: true }));*/
 app.use(express.json());
-/*app.set('view engine', 'ejs');*/
 
-/*WebResourcesService.getCurrentLocation().then(async function() {
-  app.locals.categories = await CategoryService.getCategories();
-  startServer();
-});*/
-
-startServer();
-function startServer() {
-  app.listen(port, function() {
-    console.log('Server running at port 3000: http://localhost:3000');
-  });
-}
-
-
-// GET method for home page
+// GET method to retrieve all products
 app.get('/products', async function(request, response) {
   let products = await ProductService.getProducts();
   response.status(200).send(products);
 });
 
+// GET method to retrieve a product given its id
+app.get('/products/:id', async function(request, response) {
+  let id = request.params.id;
+  let product = await ProductService.getProduct(id);
+  response.status(200).send(product);
+});
 
+app.listen(port, function() {
+  console.log('Server running at port 3000: http://localhost:3000');
+});
 
 // GET method to prevent 404 not found
 /*app.get('*', function(request, response) { response.redirect('/') });*/
