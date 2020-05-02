2058927cd996c23d25541e14db6bc39cc1fe70dd
Romina Villaverde <romina.villaverde@gmail.com>
2020-05-01 19:58:31 -0500

    sequelize, create order

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var cors = require('cors');
-const port =  process.env.PORT || 3000;
+const port =  process.env.PORT || 8080;
 
 const path = require('path');
 
@@ -10,7 +10,10 @@ const path = require('path');
 let app = express();
 app.use(cors());
 
+const CustomerService = require('./lib/customerService.js');
 const ProductService = require('./lib/productService.js');
+const OrderService = require('./lib/orderService.js');
+const ShippingService = require('./lib/shippingService.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -27,6 +30,51 @@ app.get('/products/:id', async function(request, response) {
   let id = request.params.id;
   let product = await ProductService.getProduct(id);
   response.status(200).send(product);
+});
+
+// GET method to retrieve all customers
+app.get('/customers', async function(request, response) {
+  let customers = await CustomerService.getCustomers();
+  response.status(200).send(customers);
+});
+
+// GET method to retrieve a customer given its id
+app.get('/customers/:id', async function(request, response) {
+  let id = request.params.id;
+  let customer = await CustomerService.getCustomer(id);
+  response.status(200).send(customer);
+});
+
+// GET method to retrieve all orders
+app.get('/orders', async function(request, response) {
+  let orders = await OrderService.getOrders();
+  response.status(200).send(orders);
+});
+
+// GET method to retrieve a order given its id
+app.get('/orders/:id', async function(request, response) {
+  let id = request.params.id;
+  let order = await OrderService.getOrder(id);
+  response.status(200).send(order);
+});
+
+// POST method to create an order
+app.post('/orders', async function(request, response) {
+  let customer = request.body.customer;
+  let products = request.body.products;
+
+  let order = {};
+  order.customer_id = await CustomerService.createCustomer(customer);
+  order.shipping_fee = ShippingService.calculateShipping(customer.postal_code);
+  order.order_id = await OrderService.createOrder(order, products);
+
+  response.status(200).send({ order_id: order.order_id });
+});
+
+app.get('/shipping', async function(request, response) {
+  let postalCode = request.query.postal_code;
+  let shippingFee = ShippingService.calculateShipping(postalCode);
+  response.status(200).send({ fee: shippingFee });
 });
 
 app.listen(port, function() {
